3e36a716adf42e588601759d1af053e9a978e2cf
zoekao <kaoxx085@umn.edu>
2020-01-03 19:53:42 -0600

    organize make_data.R file

---
+++ inst/make_data.R
@@ -0,0 +1,7 @@
+
+lifetable <- read.csv("data/lifetable.csv")
+behavior <- read.csv("data/TanningBehavior.csv")
+
+ONtan <- list("lifetable" = lifetable, "behavior" = behavior)
+
+usethis::use_data(ONtan, overwrite = T)
