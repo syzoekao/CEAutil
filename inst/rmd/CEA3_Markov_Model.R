b5d2cfa2f13bbaf86ce25b6082b0afd294a7e9bb
zoekao <kaoxx085@umn.edu>
2020-03-31 22:08:47 -0500

    updated webpage

--- inst/rmd/CEA3_Markov_Model.R
+++ inst/rmd/CEA3_Markov_Model.R
@@ -8,6 +8,15 @@ library(CEAutil)
 library(dampack)
 
 #### 1. Read in, set, or transform parameters if needed
+
+p_nontan_to_cancer <- 0.005
+p_regtan_to_cancer <- 0.04
+p_cancer_to_dead <- 0.07
+n_worker <- 1000
+salary <- 28000
+target_red <- 0.1
+universal_red <- 0.8
+
 data(ONtan)
 ltable <- ONtan$lifetable
 behavior <- ONtan$behavior
@@ -15,32 +24,16 @@ behavior <- ONtan$behavior
 print(head(ltable))
 print(head(behavior))
 
+n_t <- 100
+state_names <- c("nontan", "regtan", "cancer", "deadnature", "deadcancer")
+v_init <- c(1, 0, 0, 0, 0)
 
-l_param_all <- list(p_nontan_to_cancer = 0.005,
-                    p_regtan_to_cancer = 0.04,
-                    p_cancer_to_dead = 0.07,
-                    n_worker = 1000,
-                    salary = 28000,
-                    target_red = 0.1,
-                    universal_red = 0.8,
-                    ltable = ltable,
-                    behavior = behavior,
-                    state_names = c("nontan", "regtan", "cancer", "deadnature", "deadcancer"),
-                    n_t = 100,
-                    v_init = c(1, 0, 0, 0, 0))
-
-
-other_input_ls <- list(ltable = ltable,
-                       behavior = behavior,
-                       state_names = c("nontan", "regtan", "cancer", "deadnature", "deadcancer"),
-                       n_t = 100,
-                       v_init = c(1, 0, 0, 0, 0))
 
 # We need the age index for matching values of the lifetable and behavior data.
-ages <- c(10 : (10 + l_param_all$n_t - 1))
+ages <- c(10 : (10 + n_t - 1))
 
 # We extract the probability of natural death age 10-110 from the lifetable.
-p_mort <- ltable$qx[match(ages, other_input_ls$ltable$age)]
+p_mort <- ltable$qx[match(ages, ltable$age)]
 
 # Strategy
 ## We modify the values of tanning behavior based on strategy of interest.
@@ -53,15 +46,15 @@ if (strategy == "universal_ban") {
 }
 
 # We extract the tanning behavior at age 10-110 from the behavior data
-p_init_tan <- other_input_ls$behavior$p_init_tanning[match(ages, other_input_ls$behavior$age)]
-p_halt_tan <- other_input_ls$behavior$p_halt_tanning[match(ages, other_input_ls$behavior$age)]
+p_init_tan <- behavior$p_init_tanning[match(ages, behavior$age)]
+p_halt_tan <- behavior$p_halt_tanning[match(ages, behavior$age)]
 
 # We get the number of health states based on the length of the string vector, state_names
-state_names <- l_param_all$state_names
-n_states <- length(l_param_all$state_names)
+state_names <- state_names
+n_states <- length(state_names)
 
 # Get the time horizon
-n_t <- l_param_all$n_t
+n_t <- n_t
 
 #### 2. Create transition matrix
 tr_mat <- array(0, dim = c(n_states, n_states, n_t),
@@ -69,21 +62,21 @@ tr_mat <- array(0, dim = c(n_states, n_states, n_t),
 
 # 1. Fill out the transition probabilities from non-tanner to other states
 tr_mat["nontan", "regtan", ] <- p_init_tan
-tr_mat["nontan", "cancer", ] <- l_param_all$p_nontan_to_cancer
+tr_mat["nontan", "cancer", ] <- p_nontan_to_cancer
 tr_mat["nontan", "deadnature", ] <- p_mort
-tr_mat["nontan", "nontan", ] <- 1 - p_init_tan - l_param_all$p_nontan_to_cancer - p_mort
+tr_mat["nontan", "nontan", ] <- 1 - p_init_tan - p_nontan_to_cancer - p_mort
 
 # 2. Fill out the transition probabilities from regular tanner to other states
 tr_mat["regtan", "nontan", ] <- p_halt_tan
-tr_mat["regtan", "cancer", ] <- l_param_all$p_regtan_to_cancer
+tr_mat["regtan", "cancer", ] <- p_regtan_to_cancer
 tr_mat["regtan", "deadnature", ] <- p_mort
-tr_mat["regtan", "regtan", ] <- 1 - p_halt_tan - l_param_all$p_regtan_to_cancer - p_mort
+tr_mat["regtan", "regtan", ] <- 1 - p_halt_tan - p_regtan_to_cancer - p_mort
 
 # 3. Fill out the transition probabilities from skin cancer to other states.
 #    Be careful that this is a tunnel state. Therefore, there is no self loop.
-tr_mat["cancer", "deadcancer", ] <- l_param_all$p_cancer_to_dead
+tr_mat["cancer", "deadcancer", ] <- p_cancer_to_dead
 tr_mat["cancer", "deadnature", ] <- p_mort
-tr_mat["cancer", "nontan", ] <- 1 - l_param_all$p_cancer_to_dead - p_mort
+tr_mat["cancer", "nontan", ] <- 1 - p_cancer_to_dead - p_mort
 
 # 4. Fill out the transition probabilities for cancer specific death (this is an absorbing state!!)
 tr_mat["deadcancer", "deadcancer", ] <- 1
@@ -108,7 +101,7 @@ if (any(rowSums(t(apply(tr_mat, 3, rowSums))) != n_states)) stop("transition pro
 trace_mat <- matrix(0, ncol = n_states, nrow = n_t + 1,
                     dimnames = list(c(10 : (10 + n_t)), state_names))
 # Modify the first row of the trace_mat using the v_init
-trace_mat[1, ] <- l_param_all$v_init
+trace_mat[1, ] <- v_init
 
 # Suppose that we want to track the cost of having skin cancer for a year
 trace_cost <- rep(0, n_t)
